61cf42512dd82d041f69c1203f3690a64610f5a3
Dinika <dinikasaxenas@gmail.com>
2020-04-03 12:57:59 +0200

    Support pagination by sending current page number in request
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -57,7 +57,7 @@ class Feed extends Component {
     const query = {
       query: `
       {
-        getPosts {
+        getPosts(page: ${page}) {
           posts {
             _id
             title
@@ -202,7 +202,18 @@ class Feed extends Component {
           createdAt: resData.data.createPost.createdAt
         };
         this.setState(prevState => {
+          let updatedPosts = [...prevState.posts]
+          if (prevState.editPost) {
+            const postIndex = prevState.posts.findIndex(
+              p => p._id === prevState.editPost._id
+            )
+            updatedPosts[postIndex] = post
+          } else {
+            updatedPosts.pop()
+            updatedPosts.unshift(post)
+          }
           return {
+            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false
